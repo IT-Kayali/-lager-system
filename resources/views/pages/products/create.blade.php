39d63dce2cd619de0f3d887c5f20340ec067dd68
Lager Admin <admin@lager.local>
2026-07-18 17:31:23 +0000

    Refresh product editor UI

--- resources/views/pages/products/create.blade.php
+++ resources/views/pages/products/create.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.premium title="Produkt hinzufügen" subtitle="Produktdaten anlegen. Preise werden nicht hier gepflegt.">
-    <section class="premium-card">
-        <form method="POST" action="{{ route('products.store') }}" enctype="multipart/form-data">
+    <section class="premium-card product-editor-card">
+        <form class="product-editor-form" method="POST" action="{{ route('products.store') }}" enctype="multipart/form-data">
             @include('pages.products._form')
         </form>
     </section>
--- resources/views/pages/products/edit.blade.php
+++ resources/views/pages/products/edit.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.premium title="Produkt bearbeiten" subtitle="Produktdaten bearbeiten. Preise bleiben ausschließlich im Preise-Tab.">
-    <section class="premium-card">
-        <form method="POST" action="{{ route('products.update', $product) }}" enctype="multipart/form-data">
+    <section class="premium-card product-editor-card">
+        <form class="product-editor-form" method="POST" action="{{ route('products.update', $product) }}" enctype="multipart/form-data">
             @method('PUT')
             @include('pages.products._form')
         </form>
