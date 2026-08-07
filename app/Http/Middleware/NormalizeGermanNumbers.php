d29cd6203352941b18bd85468be1b993aac468db
IT-Kayali <g.kayali@it-kayali.de>
2026-08-08 01:27:48 +0200

    Normalize German decimal input centrally

---
+++ app/Http/Middleware/NormalizeGermanNumbers.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Support\GermanNumber;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class NormalizeGermanNumbers
+{
+    private const DECIMAL_KEYS = [
+        'quantity',
+        'minimum_stock',
+        'price',
+        'amount',
+        'shipping_price_gross',
+    ];
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->isMethodSafe()) {
+            return $next($request);
+        }
+
+        $request->merge($this->normalizeArray($request->all()));
+
+        return $next($request);
+    }
+
+    private function normalizeArray(array $values, array $path = []): array
+    {
+        foreach ($values as $key => $value) {
+            $currentPath = [...$path, (string) $key];
+
+            if (is_array($value)) {
+                $values[$key] = $this->normalizeArray($value, $currentPath);
+                continue;
+            }
+
+            if ($this->shouldNormalize((string) $key, $path)) {
+                $values[$key] = GermanNumber::parse($value);
+            }
+        }
+
+        return $values;
+    }
+
+    private function shouldNormalize(string $key, array $parentPath): bool
+    {
+        if (in_array($key, self::DECIMAL_KEYS, true)) {
+            return true;
+        }
+
+        return in_array('prices', $parentPath, true);
+    }
+}
