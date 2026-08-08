17dc4719078a2a01d98cf8747c8de6f147f210b8
IT-Kayali <g.kayali@it-kayali.de>
2026-08-08 04:09:27 +0200

    Add reusable browser branding head partial

---
+++ resources/views/partials/browser-branding.blade.php
@@ -0,0 +1,18 @@
+@php
+    $browserSiteName = \App\Models\ApplicationSetting::siteName();
+    $browserFaviconPath = \App\Models\ApplicationSetting::siteFaviconPath();
+    $browserPageTitle = trim((string) ($pageTitle ?? ''));
+    $browserDocumentTitle = $browserPageTitle !== ''
+        ? $browserPageTitle . ' · ' . $browserSiteName
+        : $browserSiteName;
+@endphp
+
+<title>{{ $browserDocumentTitle }}</title>
+
+@if ($browserFaviconPath)
+    <link rel="icon" href="{{ route('site.favicon', [], false) }}?v={{ md5($browserFaviconPath) }}" sizes="any">
+@else
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+@endif
