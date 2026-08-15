a0b8a97c89a7e00829cdfdc27204e83f3a705826
IT-Kayali <g.kayali@it-kayali.de>
2026-08-16 00:49:12 +0200

    Post sales branch withdrawals to sales route

--- resources/views/pages/branch-withdrawals/create.blade.php
+++ resources/views/pages/branch-withdrawals/create.blade.php
@@ -1,5 +1,11 @@
+@php
+    $storeRoute = auth()->user()?->isSales()
+        ? 'sales.branch-withdrawals.store'
+        : 'branch-withdrawals.store';
+@endphp
+
 <x-layouts.premium title="Filialausgang erstellen" subtitle="Mehrere Produkte für eine Filiale vorbereiten und den Ausgabestatus verwalten.">
-    <form class="branch-editor-form" method="POST" action="{{ route('branch-withdrawals.store') }}">
+    <form class="branch-editor-form" method="POST" action="{{ route($storeRoute) }}">
         @include('pages.branch-withdrawals._form', ['submitLabel' => 'Filialausgang speichern'])
     </form>
 </x-layouts.premium>
