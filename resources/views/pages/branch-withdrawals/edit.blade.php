f83275b6413a58e48490f06b97f8182d41a269a1
IT-Kayali <ghaithkayali99@gmail.com>
2026-09-06 14:51:56 +0200

    Use sales branch update route when editing

--- resources/views/pages/branch-withdrawals/edit.blade.php
+++ resources/views/pages/branch-withdrawals/edit.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.premium title="Filialausgang bearbeiten" subtitle="Produkte, Mengen, Filiale und Status sicher aktualisieren.">
-    <form class="branch-editor-form" method="POST" action="{{ route('branch-withdrawals.update', $withdrawal) }}">
+    <form class="branch-editor-form" method="POST" action="{{ route(auth()->user()?->isSales() ? 'sales.branch-withdrawals.update' : 'branch-withdrawals.update', $withdrawal) }}">
         @method('PUT')
         @include('pages.branch-withdrawals._form', ['submitLabel' => 'Änderungen speichern'])
     </form>
