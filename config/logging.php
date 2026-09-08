03bbd79a24e9bda71467b0ac721c037028f17b5b
IT-Kayali <ghaithkayali99@gmail.com>
2026-09-08 20:06:01 +0200

    Add dedicated CSP report log channel

--- config/logging.php
+++ config/logging.php
@@ -73,6 +73,14 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'csp' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/csp.log'),
+            'level' => 'warning',
+            'days' => 7,
+            'replace_placeholders' => true,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
