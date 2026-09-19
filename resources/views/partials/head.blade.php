5783bb5e0187ef40bbf89b57567b131a2886b4ff
IT-Kayali <ghaithkayali99@gmail.com>
2026-09-19 17:25:30 +0200

    Security: load bulk styles in shared head

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,4 +6,6 @@
 @fonts
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+<link rel="stylesheet" href="{{ asset('css/csp-static-bulk.css') }}">
+<link rel="stylesheet" href="{{ route('application.theme.css') }}">
 @fluxAppearance
